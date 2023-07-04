360eafce4b295da956c241c6e41919435a415ad0
Claudia Rodrigues <claudia.raphael73@gmail.com>
2023-07-04 17:35:10 -0300

    add story

--- lotr_dynamic_story.py
+++ lotr_dynamic_story.py
@@ -21,7 +21,10 @@ actions = ["slay", "kiss", "save", "marry", "rescue", "eat", "study", "talk with
 places = ["Old Forrest", "Lothlórien", "Fangorn", "Beleriand", "Valinor"]
 
 # Story
-things = choice(things)
+thing = choice(things)
 friend = choice(friends)
 action = choice(actions)
 place = choice(places)
+
+story = "Once upon a time, there was an Elf called " + name + ". The Elf was " + description + " in Middle Earth." + " It liked nothing better than to " +  action + " " + thing + ". Sadly, the Elf was so great at this that it ran out of " + thing + " to " + action + " in " + place + ". The Elf became very bored. Luckily the Elf had a friend called " + friend + ". " + friend + " knew where the Elf could find lots of " + thing + " and the two of them travelled far away from " + place + " and found a land filled with lots of lovely " + thing + " to " + action + ". " + name + " and " + friend + " lived happily ever, with all the " + thing + " they wanted."
+print(story)
